a085020a3ff1a71332b01ac5e42ba43a261d01f2
taobo <taobo@TBMac-mini.local>
2017-03-09 17:40:41 +0800

    添加文件

---
+++ BlackMad/blackmad/MainCell.h
@@ -0,0 +1,13 @@
+//
+//  MainCell.h
+//  blackmad
+//
+//  Created by taobo on 17/3/9.
+//  Copyright © 2017年 rongyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainCell : UITableViewCell
+
+@end
---
+++ BlackMad/blackmad/MainCell.m
@@ -0,0 +1,24 @@
+//
+//  MainCell.m
+//  blackmad
+//
+//  Created by taobo on 17/3/9.
+//  Copyright © 2017年 rongyun. All rights reserved.
+//
+
+#import "MainCell.h"
+
+@implementation MainCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
