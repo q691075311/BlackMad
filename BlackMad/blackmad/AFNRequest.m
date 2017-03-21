84d216ace79a49cc9c03f092719a08a048256cec
陶博 <691075311@qq.com>
2017-03-21 18:04:59 +0800

    5432

--- BlackMad/blackmad/AFNRequest.m
+++ BlackMad/blackmad/AFNRequest.m
@@ -13,31 +13,11 @@
 + (void)requestWithDataURL:(NSString *)URL WithUserName:(NSString *)userName WithPwsd:(NSString *)pwd WithComplete:(void (^)(NSArray *))block{
     AFHTTPSessionManager * manager = [AFHTTPSessionManager manager];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-<<<<<<< HEAD
-    NSString * str = @"http://116.62.7.43/d/phone/register";
-    NSString * url = [str stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
-    //设置参数
-=======
     //请求URL
     NSString * url = [[NSString stringWithFormat:@"%@%@",BASEURL,URL] stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
-    //设置请求参数
->>>>>>> 8e37b5bb3d505ffba5a7fccbbc30e4d2db7a77a0
-    NSMutableDictionary * dic = [[NSMutableDictionary alloc] init];
-    [dic setValue:userName forKey:@"username"];
-    [dic setValue:pwd forKey:@"password"];
-    [dic setValue:@"ios" forKey:@"regDevice"];
-<<<<<<< HEAD
-    //字典转为Json字符串
-    NSString * parameters = [self dictionaryToJson:dic];
     //发起请求
-    [manager POST:url parameters:parameters progress:^(NSProgress * _Nonnull uploadProgress) {
-        NSLog(@"%@",uploadProgress);
-=======
-    NSString * parameters = [self dictionaryToJson:dic];
-    //发起请求
-    [manager POST:url parameters:parameters progress:^(NSProgress * _Nonnull uploadProgress) {
+    [manager POST:url parameters:nil progress:^(NSProgress * _Nonnull uploadProgress) {
         
->>>>>>> 8e37b5bb3d505ffba5a7fccbbc30e4d2db7a77a0
     } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         NSArray * arr = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
         NSLog(@"%@",arr);
